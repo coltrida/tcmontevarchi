9c21ec83680a21b01660adf309e53904fa1c176a
marco <coltricat75@gmail.com>
2020-04-12 22:42:56 +0200

    api cancellazione2

--- resources/js/components/pannelloUser/cancellaOra.vue
+++ resources/js/components/pannelloUser/cancellaOra.vue
@@ -4,6 +4,21 @@
 
 <script>
 export default {
-    
+    data: () => ({
+      
+      cancella:{
+      username1: '',
+      username2: '',
+      username3: '',
+      username4: '',
+      campo: '',
+      dataprenotazione: '',
+      oraon: '',
+      full: '',
+      doppio: ''
+      },
+  created(){
+      axios.get('/api/prenotazioni/')
+  }  
 }
 </script>
